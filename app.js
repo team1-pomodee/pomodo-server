1e8448bb9c0150b318a6d2d3bf7288aafa77e475
Minte Temple <mintetemple@gmail.com>
2022-02-05 14:44:18 -0800

    chore: cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
+  transports : ['websocket'],
   cors: {
    origin: "https://pomododee.netlify.app/",
     methods: ["GET", "POST"]
