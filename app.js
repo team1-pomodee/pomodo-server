e2d0a73b9a6a8f061a126526bc68cbe85cc4b632
Wing <38932460+wingbui@users.noreply.github.com>
2022-02-10 13:54:05 -0800

    group socket.io part to handle it later on

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
+
+// SOCKET.IO starts
+// Will work on this part later on
+const { Server } = require('socket.io');
 const io = new Server(server, {
-  transports : ['websocket'],
+  transports: ['websocket'],
   cors: {
-   origin: "https://pomododee.netlify.app/",
-    methods: ["GET", "POST"]
-}});
-
-
-app.get("/", (req, res) => {
-  res.status(200).send("Hello world")
-})
+    origin: 'https://pomododee.netlify.app/',
+    methods: ['GET', 'POST'],
+  },
+});
 
 io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('disconnect', () => {
     console.log('user disconnected');
-   });
-  
+  });
+
   socket.on('action', (msg) => {
-    console.log(msg)
-    handleSendSignal(msg)
+    console.log(msg);
+    handleSendSignal(msg);
   });
 });
 
 const handleSendSignal = (signal) => {
   io.emit('action', signal);
-}
+};
+// SOCKET.IO ends
 
-server.listen(process.env.PORT || 4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log('listening on *:4000');
 });
