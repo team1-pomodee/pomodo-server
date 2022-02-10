7f574cda5a8a9b2b14d229ba2388cd0b9d4571ef
Wing <38932460+wingbui@users.noreply.github.com>
2022-02-10 13:57:46 -0800

    move socket.io part to the bottom to handle later

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const server = app.listen(process.env.PORT || 4000, () => {
+  console.log('listening on *:4000');
+});
+
 // SOCKET.IO starts
 // Will work on this part later on
 const { Server } = require('socket.io');
@@ -28,7 +32,3 @@ const handleSendSignal = (signal) => {
   io.emit('action', signal);
 };
 // SOCKET.IO ends
-
-app.listen(process.env.PORT || 4000, () => {
-  console.log('listening on *:4000');
-});
