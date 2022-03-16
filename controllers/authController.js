4715dbcb62264ec577a63777a846a147fb5b2e79
Simer0996 <singhsimer09@gmail.com>
2022-03-16 14:26:56 -0700

    updating the total number of cycles 's logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,23 @@
-import User from "../models/User.js";
-import { StatusCodes } from "http-status-codes";
-import bcrypt from "bcryptjs";
+import User from "../models/User.js"
+import { StatusCodes } from "http-status-codes"
+import bcrypt from "bcryptjs"
 
 const register = async (req, res) => {
-  const { username, email, password } = req.body;
+  const { username, email, password } = req.body
 
   if (!username || !email || !password) {
-    throw new Error("Please provide username, email and password.");
+    throw new Error("Please provide username, email and password.")
   }
 
-  const pass = await bcrypt.hash(password, 10);
+  const pass = await bcrypt.hash(password, 10)
 
   try {
     User.find({ username, email }).exec(async (error, result) => {
       if (error) {
-        throw new Error(error);
+        throw new Error(error)
       } else {
         if (result.length === 0) {
-          const user = await User.create({ username, email, password: pass });
+          const user = await User.create({ username, email, password: pass })
           res.status(StatusCodes.CREATED).send({
             user: {
               username: user.username,
@@ -25,86 +25,89 @@ const register = async (req, res) => {
               cycles: user.cycles,
               cycleDetail: user.cycleDetail,
             },
-          });
+          })
         } else {
-          res.status(200).json({ message: "User already exist" });
+          res.status(200).json({ message: "User already exist" })
         }
       }
-    });
+    })
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error)
   }
-};
+}
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
   if (!email || !password) {
-    throw new Error("Please provide email and password.");
+    throw new Error("Please provide email and password.")
   }
 
   try {
-    const user = await User.findOne({ email }).select("+password");
+    const user = await User.findOne({ email }).select("+password")
 
     if (!user) {
-      throw new Error("You are trying to login with invalid credentials.");
+      throw new Error("You are trying to login with invalid credentials.")
     }
 
-    const isPasswordCorrect = await user.comparePasswords(password);
+    const isPasswordCorrect = await user.comparePasswords(password)
 
     if (!isPasswordCorrect) {
-      throw new Error("You are trying to login with invalid credentials.");
+      throw new Error("You are trying to login with invalid credentials.")
     }
 
-    user.password = undefined;
-    const token = user.createJWT();
+    user.password = undefined
+    const token = user.createJWT()
 
-    res.status(StatusCodes.OK).json({ user, token });
+    res.status(StatusCodes.OK).json({ user, token })
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error)
   }
-};
+}
 
 const updateUser = async (req, res) => {
-  const { username, cycles } = req.body;
-  const user = await User.findOne({ username });
-  const updated = await user.update({ cycles });
-  res.send(`${updated}`);
-
-  // res.send('Update User');
-};
-
-const addCycle = async (req, res) => {
-  const { email, cycleData } = req.body;
-  User.findOne({ email: email })
-    .exec()
-    .then((result) => {
-      result.update({ $push: { cycleDetail: cycleData } }).exec();
-    })
-    .then((result) => {
-      res.send("Cycle added");
-    })
-    .catch((error) => {
-      res.status(500).json(error);
-    });
-};
-
-
-const updateAvatar = async (req, res) => {
-  const { username, avatar } = req.body;
+  const { username, cycles } = req.body
   User.findOne({ username: username })
     .exec()
     .then((result) => {
-      result.update({ avatar: avatar }).exec();
+      result.update({ cycles: cycles }).exec()
     })
     .then((result) => {
-      res.send({message: 'UPDATE_SUCCESSFUL', data: result});
+      res.send("Update User")
     })
     .catch((error) => {
-       throw new Error(error);
-    });
-};
+      console.error(error)
+    })
+}
 
+const addCycle = async (req, res) => {
+  const { email, cycleData } = req.body
+  User.findOne({ email: email })
+    .exec()
+    .then((result) => {
+      result.update({ $push: { cycleDetail: cycleData } }).exec()
+    })
+    .then((result) => {
+      res.send("Cycle added")
+    })
+    .catch((error) => {
+      res.status(500).json(error)
+    })
+}
 
+const updateAvatar = async (req, res) => {
+  const { username, avatar } = req.body
+  User.findOne({ username: username })
+    .exec()
+    .then((result) => {
+      result.update({ avatar: avatar }).exec()
+    })
+    .then((result) => {
+      res.send({ message: "UPDATE_SUCCESSFUL", data: result })
+    })
+    .catch((error) => {
+      throw new Error(error)
+    })
+}
 
-export { register, updateAvatar, login, updateUser, addCycle };
+export { register, updateAvatar, login, updateUser, addCycle }
