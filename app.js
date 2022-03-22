a5910834298756a03ad32f5bf6c28340b4078d48
Simer Singh <simersingh@Simers-MacBook-Air.local>
2022-03-22 00:54:04 -0700

    updating

--- app.js
+++ app.js
@@ -1,154 +1,148 @@
-import express from 'express';
-const app = express();
-import * as http from 'http';
-const server = http.createServer(app);
-import { Server } from "socket.io";
+import express from "express"
+const app = express()
+import * as http from "http"
+const server = http.createServer(app)
+import { Server } from "socket.io"
 
-import cors from 'cors'
+import cors from "cors"
 
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from "dotenv"
+dotenv.config()
 
-import 'express-async-errors';
+import "express-async-errors"
 
 // database
-import connectDB from './db/connect.js';
+import connectDB from "./db/connect.js"
 
 // router
-import authRouter from './routes/authRoutes.js';
-import requestRouter from './routes/requestRouter.js';
-import friendRouter from './routes/friendRoutes.js';
+import authRouter from "./routes/authRoutes.js"
+import requestRouter from "./routes/requestRouter.js"
+import friendRouter from "./routes/friendRoutes.js"
 
 // middleware
-import notFoundMiddleware from './middleware/not-found.js';
-import errorHandlerMiddleware from './middleware/error-handler.js';
-
+import notFoundMiddleware from "./middleware/not-found.js"
+import errorHandlerMiddleware from "./middleware/error-handler.js"
 
 app.use(cors())
-app.use(express.json());
+app.use(express.json())
 
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000
 
 const io = new Server(server, {
   cors: {
-   origin: '*',
-    methods: ['GET, POST, OPTIONS, PUT, PATCH, DELETE'],
-
-}});
- const rooms = {}
- const usersList = {}
-
-app.get('/', (req, res) => {
-  res.send('Welcome to pomodee server')
+    origin: "*",
+    methods: ["GET, POST, OPTIONS, PUT, PATCH, DELETE"],
+  },
 })
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/request', requestRouter);
-app.use('/api/v1/friend', friendRouter);
+const rooms = {}
+const usersList = {}
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.get("/", (req, res) => {
+  res.send("Welcome to pomodee server")
+})
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/request", requestRouter)
+app.use("/api/v1/friend", friendRouter)
 
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
-io.on('connection', (socket) => {
-
+io.on("connection", (socket) => {
   //what happens when a user refresh and looses their room name?
   //what happens when a user disconnects
   //how do users join room
 
-  
-  console.log('a user connected');
+  console.log("a user connected")
 
-  socket.on('disconnect', () => {
-    const room = usersList[socket.id] 
+  socket.on("disconnect", () => {
+    const room = usersList[socket.id]
     const users = rooms[room] || []
 
     const onlineUsers = users.filter((data) => data.id !== socket.id)
     if (onlineUsers.length > 0) {
-      rooms[room] = onlineUsers;
-      io.to(onlineUsers[0].roomName).emit("new user", onlineUsers)  
+      rooms[room] = onlineUsers
+      io.to(onlineUsers[0].roomName).emit("new user", onlineUsers)
     }
-    console.log('disconnected')
-  
-  });
+    console.log("disconnected")
+  })
 
-  socket.on('action', (msg) => {
+  socket.on("action", (msg) => {
     console.log(msg)
     handleSendSignal(msg)
-  });
+  })
 
-
-  socket.on('join room', (user) => {
+  socket.on("join room", (user) => {
     // console.log(user)
     if (user.roomName) {
-      socket.join(user.roomName);
+      socket.join(user.roomName)
       const userDetails = {
         ...user,
         id: socket.id,
       }
 
-        rooms[user.roomName] =  rooms[user.roomName] || []
+      rooms[user.roomName] = rooms[user.roomName] || []
       const users = rooms[user.roomName]
       const isUserExist = users.find((data) => data.username === user.username)
 
-      
       if (!isUserExist) {
         users.push(userDetails)
       }
-      
+
       socket.emit("new user", users)
       usersList[socket.id] = user.roomName
-      
-      console.log(`${user.username} has joined the ${user.roomName} room`, users.length)
+
+      console.log(
+        `${user.username} has joined the ${user.roomName} room`,
+        users.length
+      )
     }
 
     let interval
-    let count = 0;
-    let cycles = 0;
+    let count = 0
+    let cycles = 0
 
-    socket.on('timer', (control) => { 
+    socket.on("timer", (control) => {
       if (control.action === "START") {
-        interval = setInterval(() => { 
-          rooms[user.roomName].count = rooms[user.roomName].count + 1;
-          io.to(control.roomName).emit("timer", {count, cycles})
+        interval = setInterval(() => {
+          rooms[user.roomName].count = rooms[user.roomName].count + 1
+          io.to(control.roomName).emit("timer", { count, cycles })
         }, 1000)
       }
 
       if (control.action === "STOP") {
-        clearInterval(interval);
-        rooms[user.roomName].count = 0;
-         io.to(control.roomName).emit("timer", {count: 0, cycles: 0})
+        clearInterval(interval)
+        rooms[user.roomName].count = 0
+        io.to(control.roomName).emit("timer", { count: 0, cycles: 0 })
       }
 
       if (control.action === "PAUSE") {
-        clearInterval(interval);      
-         io.to(control.roomName).emit("timer", {count: rooms[user.roomName].count, cycles: 0})
+        clearInterval(interval)
+        io.to(control.roomName).emit("timer", {
+          count: rooms[user.roomName].count,
+          cycles: 0,
+        })
       }
-      
-      if (control.action === "RESET") {
-        clearInterval(interval);
-        io.to(control.roomName).emit("timer", {count: 0, cycles: 0})
-      }
-      
-    })
-  });
 
-});
+      if (control.action === "RESET") {
+        clearInterval(interval)
+        io.to(control.roomName).emit("timer", { count: 0, cycles: 0 })
+      }
+    })
+  })
+})
 
 const handleSendSignal = (signal) => {
-  io.emit('action', signal);
+  io.emit("action", signal)
 }
 
-
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URL);
-        server.listen(port, () => {
-          console.log(`Server is listening to port ${port}`);
-        }); 
-    } catch (error) {
-        console.log(error);
-    }
-};
-start();
-
+  try {
+    await connectDB(process.env.MONGO_URL)
+    server.listen(port, () => {
+      console.log(`Server is listening to port ${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+start()
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,23 @@
-import User from "../models/User.js";
-import { StatusCodes } from "http-status-codes";
-import bcrypt from "bcryptjs";
+import User from "../models/User.js"
+import { StatusCodes } from "http-status-codes"
+import bcrypt from "bcryptjs"
 
 const register = async (req, res) => {
-  const { username, email, password } = req.body;
+  const { username, email, password } = req.body
 
   if (!username || !email || !password) {
-    throw new Error("Please provide username, email and password.");
+    throw new Error("Please provide username, email and password.")
   }
 
-  const pass = await bcrypt.hash(password, 10);
+  const pass = await bcrypt.hash(password, 10)
 
   try {
     User.find({ username, email }).exec(async (error, result) => {
       if (error) {
-        throw new Error(error);
+        throw new Error(error)
       } else {
         if (result.length === 0) {
-          const user = await User.create({ username, email, password: pass });
+          const user = await User.create({ username, email, password: pass })
           res.status(StatusCodes.CREATED).send({
             user: {
               username: user.username,
@@ -25,74 +25,74 @@ const register = async (req, res) => {
               cycles: user.cycles,
               cycleDetail: user.cycleDetail,
             },
-          });
+          })
         } else {
-          res.status(200).json({ message: "User already exist" });
+          res.status(200).json({ message: "User already exist" })
         }
       }
-    });
+    })
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error)
   }
-};
+}
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
   if (!email || !password) {
-    throw new Error("Please provide email and password.");
+    throw new Error("Please provide email and password.")
   }
 
   try {
-    const user = await User.findOne({ email }).select("+password");
+    const user = await User.findOne({ email }).select("+password")
 
     if (!user) {
-      throw new Error("You are trying to login with invalid credentials.");
+      throw new Error("You are trying to login with invalid credentials.")
     }
 
-    const isPasswordCorrect = await user.comparePasswords(password);
+    const isPasswordCorrect = await user.comparePasswords(password)
 
     if (!isPasswordCorrect) {
-      throw new Error("You are trying to login with invalid credentials.");
+      throw new Error("You are trying to login with invalid credentials.")
     }
 
-    user.password = undefined;
-    const token = user.createJWT();
+    user.password = undefined
+    const token = user.createJWT()
 
-    res.status(StatusCodes.OK).json({ user, token });
+    res.status(StatusCodes.OK).json({ user, token })
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error)
   }
-};
+}
 
 const updateUser = async (req, res) => {
-  const { username, cycles } = req.body;
+  const { username, cycles } = req.body
   User.findOne({ username: username })
     .exec()
     .then((result) => {
-      result.update({ cycles: cycles }).exec();
+      result.update({ cycles: cycles }).exec()
     })
     .then((result) => {
-      res.send("Update User");
+      res.send("Update User")
     })
     .catch((error) => {
-      console.error(error);
-    });
-};
+      console.error(error)
+    })
+}
 
 const addCycle = async (req, res) => {
-  const { email, cycleData } = req.body;
+  const { email, cycleData } = req.body
 
-  const nowTimeStamp = Date.now();
-  const today = new Date(nowTimeStamp);
-  const startOfToday = today.setHours(0, 0, 0, 0);
-  const startOf6daysAgo = startOfToday - 518400000;
+  const nowTimeStamp = Date.now()
+  const today = new Date(nowTimeStamp)
+  const startOfToday = today.setHours(0, 0, 0, 0)
+  const startOf6daysAgo = startOfToday - 518400000
 
   User.findOne({ email: email })
     .exec()
     .then((result) => {
-      result.updateOne({ $push: { cycleDetail: cycleData } }).exec();
-      return result;
+      result.updateOne({ $push: { cycleDetail: cycleData } }).exec()
+      return result
     })
     .then((result) => {
       result
@@ -102,44 +102,44 @@ const addCycle = async (req, res) => {
             // Delete cycles completed before the start of 6 days ago
           },
         })
-        .exec();
+        .exec()
     })
     .then((result) => {
-      res.send("Cycle added");
+      res.send("Cycle added")
     })
     .catch((error) => {
-      res.status(500).json(error);
-    });
-};
+      res.status(500).json(error)
+    })
+}
 
 const updateAvatar = async (req, res) => {
-  const { username, avatar } = req.body;
+  const { username, avatar } = req.body
   User.findOne({ username: username })
     .exec()
     .then((result) => {
-      result.update({ avatar: avatar }).exec();
+      result.update({ avatar: avatar }).exec()
     })
     .then((result) => {
-      res.send({ message: "UPDATE_SUCCESSFUL", data: result });
+      res.send({ message: "UPDATE_SUCCESSFUL", data: result })
     })
     .catch((error) => {
-      throw new Error(error);
-    });
-};
+      throw new Error(error)
+    })
+}
 
 const hallOfFame = async (req, res) => {
   User.find({}, function (err, users) {
-    var userMap = {};
+    var userMap = {}
 
     users.forEach(function (user) {
-      userMap[user._id] = user;
-    });
+      userMap[user._id] = user
+    })
 
-    res.send(userMap);
+    res.send(userMap)
   })
     .select({ username: 1, cycles: 1 })
-    .sort({ cycles: 1 })
-    .limit(10);
-};
+    .sort({ cycles: -1 })
+    .limit(10)
+}
 
-export { register, updateAvatar, login, updateUser, addCycle, hallOfFame };
+export { register, updateAvatar, login, updateUser, addCycle, hallOfFame }
